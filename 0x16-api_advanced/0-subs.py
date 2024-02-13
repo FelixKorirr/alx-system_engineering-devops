2597f2d67c9b0f327906d6ebc16c34c397ac75a0
Felix Kipkorir <kipronofelix63@gmail.com>
2024-02-13 21:14:28 +0300

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""This script returns the number_of_subscribers for
-a given subreddit"""
+'''This script returns the number_of_subscribers for
+a given subreddit'''
 import requests
 from sys import argv
 
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a subreddit"""
-    user = {'User-Agent': 'Lizzie'}
+    '''Returns the number of subscribers for a subreddit'''
+
+    usr = {'User-Agent': 'Lizzie'}
     url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
+                       .format(subreddit), headers=usr).json()
     try:
         return url.get('data').get('subscribers')
     except Exception:
         return 0
+
 
 if __name__ == "__main__":
     number_of_subscribers(argv[1])
